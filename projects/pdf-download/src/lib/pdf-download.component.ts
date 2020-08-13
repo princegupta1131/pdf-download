cc451c2115c54145e19c904844fbb70758c118d8
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-08-13 15:33:46 +0530

    Initial commit

---
+++ projects/pdf-download/src/lib/pdf-download.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-pdf-download',
+  template: `
+    <p>
+      pdf-download works!
+    </p>
+  `,
+  styles: []
+})
+export class PdfDownloadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/pdf-download/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of pdf-download
+ */
+
+export * from './lib/pdf-download.service';
+export * from './lib/pdf-download.component';
+export * from './lib/pdf-download.module';
