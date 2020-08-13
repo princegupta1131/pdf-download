c00700fdf57b2c5ed8b5a3254c1884b309b29a62
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-08-13 15:49:19 +0530

    Added prefix

--- projects/pdf-download/src/lib/pdf-download.component.ts
+++ projects/pdf-download/src/lib/pdf-download.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-pdf-download',
+  selector: 'sb-pdf-download',
   template: `
     <p>
       pdf-download works!
